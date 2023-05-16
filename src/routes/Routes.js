65c145581f7d26ed2d68678ca64ade0b353f5b56
rinku-yo <63914098+rinku-yo@users.noreply.github.com>
2023-05-16 16:34:10 +0530

    payment Gateway page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,31 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ForgetPassword from '../pages/ForgetPassword/index';
 import PaymentGateway from '../pages/PaymentGateways/Index';
 import Accounts from '../pages/Accounts/Accounts';
-import Dashboard from '../pages/Dashboard';
-import Layout from '../pages/AppBackground';
 const Router = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route
-            path='/dashboard'
-            element=<Dashboard />
-          />
-          <Route
-            path='/accounts/:method'
-            element=<Accounts />
-          />
-          <Route
-            path='/forgetPassword'
-            element=<ForgetPassword />
-          />
-          <Route
-            path='/payment-gateway'
-            element=<PaymentGateway />
-          />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route
+          path='/accounts/:method'
+          element=<Accounts />
+        />
+        <Route
+          path='/forgetPassword'
+          element=<ForgetPassword />
+        />
+        <Route path={'/payment-gateway/'}
+        element=<PaymentGateway/>
+        />
+        <Route path={'/payment-gateway/:method'}
+        element=<PaymentGateway/>
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
