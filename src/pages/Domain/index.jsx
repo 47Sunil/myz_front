c81cb954dbf87a79c88fa6bef499f232f912170a
arshali2774 <arshali.763z@gmail.com>
2023-05-18 14:03:33 +0530

    domain half done

---
+++ src/pages/Domain/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './components/Banner';
+import AddDomain from './components/AddDomain';
+
+const Domain = () => {
+  return (
+    <div className='z-[3333] relative'>
+      <Banner />
+      <AddDomain />
+    </div>
+  );
+};
+
+export default Domain;
