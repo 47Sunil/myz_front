628ccd7c6c200786cabb82c44a0f97161f09ec54
arshali2774 <arshali.763z@gmail.com>
2023-05-25 14:31:52 +0530

    new push

--- src/pages/LandingPage/components/CreateLandingPage.jsx
+++ src/pages/LandingPage/components/CreateLandingPage.jsx
@@ -3,7 +3,11 @@ import SecondScreenWrapper from '../../../components/SecondScreen/SecondScreenWr
 import star from '../../../assets/images/star2.png';
 import templateImage from '../../../assets/images/myzer-template2.png';
 import TransparentButton from '../../../components/TransparentButton/TransparentButton';
-import { AiOutlineSwap, AiFillCaretDown } from 'react-icons/ai';
+import {
+  AiOutlineSwap,
+  AiFillCaretDown,
+  AiOutlineInfoCircle,
+} from 'react-icons/ai';
 import SecondScreenForm from '../../../components/SecondScreen/Form';
 import DynamicInputManager from '../../../components/DynamicInputManager/DynamicInputManager';
 import DropDown from '../../../components/CustomDropDown/DropDown';
@@ -12,6 +16,7 @@ import Purchase from '../../../assets/svg/Purchase';
 import Lead from '../../../assets/svg/Lead';
 import Other from '../../../assets/svg/Other';
 import magic from '../../../assets/icons/magic.png';
+import razorPayLogo from '../../../assets/icons/razorpay.png';
 import { Switch } from '@headlessui/react';
 import styled from 'styled-components';
 import SolarSvg from '../../../assets/svg/LandingPageSolar';
@@ -21,6 +26,7 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 const CreateLandingPage = () => {
   const [title, setTitle] = useState('');
   const [enabled, setEnabled] = useState(false);
+  const [paymentSelect, setPaymentSelect] = useState(false);
   const [searchParams] = useSearchParams();
   const queryClient = useQueryClient();
   const data = queryClient.getQueryData('templates');
@@ -126,71 +132,10 @@ const CreateLandingPage = () => {
                     type={'text'}
                   />
                   <div className='px-5 relative flex items-center mb-4'>
-                    {/* <DropDown
-                  menuClass={
-                    'bg-[rgba(255,255,255,0.16)] border border-solid border-[rgba(255,255,255,0.16)] rounded-[11px] p-[10px_20px] text-white w-full flex relative'
-                  }
-                  menuBtnText={'Select a Page Goal*'}
-                  menuBtnClass={'text-[15px] text-[rgba(255,255,255,0.38)]'}
-                >
-                  <Menu.Items className='absolute top-[50px] z-[20] grid grid-cols-3 gap-4 bg-[#2A2439] border border-solid border-[rgba(255,255,255,0.1)] rounded-[13px]  text-white w-full p-3 left-0'>
-                    <Menu.Item className='bg-[rgba(251,251,251,0.22)] rounded-[10px] py-2 hover:outline hover:outline-1 hover:outline-white hover:outline-offset-4'>
-                      <TooltipCard className='flex-grow bg-gradient-add-page-purple flex justify-center items-center'>
-                        <TooltipText>
-                          <button className='flex flex-col  items-center justify-center   gap-2'>
-                            <Purchase />
-                            Purchase
-                          </button>
-                        </TooltipText>
-                        <TooltipBox>
-                          Collect Payments or Sell Digital Products
-                        </TooltipBox>
-                      </TooltipCard>
-                    </Menu.Item>
-                    <Menu.Item className='bg-[rgba(251,251,251,0.22)] rounded-[10px] py-2 hover:outline hover:outline-1 hover:outline-white hover:outline-offset-4'>
-                      <TooltipCard className='flex-grow bg-gradient-add-page-orange flex justify-center items-center'>
-                        <TooltipText>
-                          <button className='flex flex-col  items-center justify-center p-4 py-5  gap-2'>
-                            <Lead />
-                            Lead
-                          </button>
-                        </TooltipText>
-                        <TooltipBox>
-                          Collect Payments or Sell Digital Products
-                        </TooltipBox>
-                      </TooltipCard>
-                    </Menu.Item>
-                    <Menu.Item className='bg-[rgba(251,251,251,0.22)] rounded-[10px] py-2 hover:outline hover:outline-1 hover:outline-white hover:outline-offset-4'>
-                      <TooltipCard className='flex-grow bg-gradient-add-page-blue flex justify-center items-center'>
-                        <TooltipText>
-                          <button className='flex flex-col  items-center justify-center p-4 py-5  gap-2'>
-                            <Other />
-                            Other
-                          </button>
-                        </TooltipText>
-                        <TooltipBox>
-                          Collect Payments or Sell Digital Products
-                        </TooltipBox>
-                      </TooltipCard>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <p className='text-white font-medium bg-[#4e4367] text-[10px] p-[2px_4px] rounded-[7px] text-center'>
-                        Collect Payments or Sell Digital Products
-                      </p>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <p className='text-white font-medium bg-[#4e4367] text-[10px] p-[2px_4px] rounded-[7px] text-center'>
-                        Collect Payments or Sell Digital Products
-                      </p>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <p className='text-white font-medium bg-[#4e4367] text-[10px] p-[2px_4px] rounded-[7px] text-center'>
-                        Collect Payments or Sell Digital Products
-                      </p>
-                    </Menu.Item>
-                  </Menu.Items>
-                </DropDown> */}
-                    <DropDown />
+                    <DropDown
+                      paymentSelect={paymentSelect}
+                      setPaymentSelect={setPaymentSelect}
+                    />
                   </div>
                   <p className='text-[rgba(255,255,255,0.67)] text-[15px] font-normal px-5 absolute top-[60%]'>
                     What Exactly you want from this Page
@@ -199,6 +144,109 @@ const CreateLandingPage = () => {
               </div>
             </SecondScreenForm>
           </div>
+          {paymentSelect && (
+            <div className='absolute inset-0 w-full h-full bg-[rgba(0,0,0,0.54)] z-30 flex justify-center items-center'>
+              <div className='bg-[#0A031B] rounded-[31px] border border-solid border-[rgba(255,255,255,0.29)] w-[60vw] h-[80vh] p-[34px_68px] overflow-y-scroll'>
+                <div className='w-full flex justify-between items-center'>
+                  <h1 className='text-white text-[24px] leading-[121%] font-medium'>
+                    Select Payment Gateway
+                  </h1>
+                  <AiOutlineInfoCircle className='text-white text-[24px] leading-[121%]' />
+                </div>
+                <hr className='w-full border border-solid border-[rgba(255,255,255,0.38)] my-[24px]' />
+                <div className='grid grid-cols-4 gap-8 '>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,255,255,0.31)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px]'>
+                    <button className='bg-gradient-landing-orange text-white text-[41px] leading-[51px] text-center h-[51px] w-[51px] font-medium rounded-full'>
+                      +
+                    </button>
+                    <p className='font-medium text-[14px] leading-[121%] text-[#bebebe] w-full text-center '>
+                      Add New Payment Gateway
+                    </p>
+                  </div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px] relative'>
+                    <div className='bg-white p-2 rounded-b-[30px] absolute top-0'>
+                      <img
+                        src={razorPayLogo}
+                        alt=''
+                      />
+                    </div>
+                    <div className='bg-white p-1 rounded-b-[30px]  invisible'>
+                      <img
+                        src={razorPayLogo}
+                        alt=''
+                      />
+                    </div>
+                    <div className='w-full text-center'>
+                      <p className='text-[14px] text-white leading-[18.9px] font-medium'>
+                        Razor Pay
+                      </p>
+                      <p className='text-[12px] text-white leading-[18.9px] '>
+                        (Yomize)
+                      </p>
+                    </div>
+                    <button className='bg-gradient-landing-orange text-white text-[14px] leading-[18.9px] text-center font-medium w-full rounded-[6px] py-1'>
+                      Select
+                    </button>
+                  </div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px] relative'>
+                    <div className='bg-white p-2 rounded-b-[30px] absolute top-0'>
+                      <img
+                        src={razorPayLogo}
+                        alt=''
+                      />
+                    </div>
+                    <div className='bg-white p-1 rounded-b-[30px]  invisible'>
+                      <img
+                        src={razorPayLogo}
+                        alt=''
+                      />
+                    </div>
+                    <div className='w-full text-center'>
+                      <p className='text-[14px] text-white leading-[18.9px] font-medium'>
+                        Razor Pay
+                      </p>
+                      <p className='text-[12px] text-white leading-[18.9px] '>
+                        (Yomize)
+                      </p>
+                    </div>
+                    <button className='bg-gradient-landing-orange text-white text-[14px] leading-[18.9px] text-center font-medium w-full rounded-[6px] py-1'>
+                      Select
+                    </button>
+                  </div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px] relative'>
+                    <div className='bg-white p-2 rounded-b-[30px] absolute top-0'>
+                      <img
+                        src={razorPayLogo}
+                        alt=''
+                      />
+                    </div>
+                    <div className='bg-white p-1 rounded-b-[30px]  invisible'>
+                      <img
+                        src={razorPayLogo}
+                        alt=''
+                      />
+                    </div>
+                    <div className='w-full text-center'>
+                      <p className='text-[14px] text-white leading-[18.9px] font-medium'>
+                        Razor Pay
+                      </p>
+                      <p className='text-[12px] text-white leading-[18.9px] '>
+                        (Yomize)
+                      </p>
+                    </div>
+                    <button className='bg-gradient-landing-orange text-white text-[14px] leading-[18.9px] text-center font-medium w-full rounded-[6px] py-1'>
+                      Select
+                    </button>
+                  </div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px]'></div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px]'></div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px]'></div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px]'></div>
+                  <div className='bg-[rgba(255,255,255,0.1)] border-2 border-dashed border-[rgba(255,107,0,0.2)] flex flex-col justify-center items-center p-[24px_15px] gap-3 h-[200px] rounded-[15px]'></div>
+                </div>
+              </div>
+            </div>
+          )}
         </SecondScreenWrapper>
       )}
     </>
