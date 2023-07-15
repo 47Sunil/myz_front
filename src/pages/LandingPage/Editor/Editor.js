be52c9aa1827105d6b157560a74b4c802b59ab7e
rinku-yo <63914098+rinku-yo@users.noreply.github.com>
2023-07-15 11:56:46 +0530

    new

--- src/pages/LandingPage/Editor/Editor.js
+++ src/pages/LandingPage/Editor/Editor.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import EditorConfig from './EditorConfig'
-import 'grapesjs/dist/css/grapes.min.css';
-import './styles.css'
-import Topbar from './Design/Topbar';
-import TwoTopbar from './Design/TwoTopbar';
-import LeftSidebar from './Design/LeftSidebar';
-import RightSidebar from './Design/RightSidebar';
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import EditorConfig from "./EditorConfig";
+import "grapesjs/dist/css/grapes.min.css";
+import "./styles.css";
+import Topbar from "./Design/Topbar";
+import TwoTopbar from "./Design/TwoTopbar";
+import LeftSidebar from "./Design/LeftSidebar";
+import RightSidebar from "./Design/RightSidebar";
 
 const Editor = () => {
+  const { id, pageType } = useParams();
+  const [editor, setEditor] = useState(null);
 
-    const {id, pageType} = useParams();
-    const [editor, setEditor] = useState(null);
+  useEffect(() => {
+    if (!editor) {
+      setEditor(EditorConfig(id, pageType));
+    }
+  });
 
-    useEffect(() => {
-        setEditor(EditorConfig(id, pageType));
-      }, []);
+  return (
+    <div className="h-screen overflow-hidden bg-gray-400/40">
+      <Topbar editor={editor} id={id} />
+      <TwoTopbar editor={editor} />
+      <LeftSidebar editor={editor} />
+      <RightSidebar editor={editor} />
+      <div id="gjs"></div>
+    </div>
+  );
+};
 
-    return (
-        <div className="h-screen overflow-hidden bg-gray-400/40">
-            <Topbar editor={editor} id={id}/>
-            <TwoTopbar editor={editor} />
-            <LeftSidebar editor={editor} />
-            <RightSidebar editor={editor} />
-            <div id="gjs"></div>
-        </div>
-    )
-}
-
-export default Editor
+export default Editor;
